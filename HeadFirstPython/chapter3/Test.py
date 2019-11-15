86c567d48a74ab03d635bac752758bfd3fee0716
yinhaijun <yinhaijun@jingdaka.com>
2019-11-15 11:26:12 +0800

    doc(chapter3.md) 添加集合数据结构笔记
    添加集合数据结构笔记

--- HeadFirstPython/chapter3/Test.py
+++ HeadFirstPython/chapter3/Test.py
@@ -3,3 +3,8 @@ print(listChar)
 person1 = {listChar[1]: ["list:1", 2, 3], 'Age': 28, 'Gender': 'Male'}
 print(person1)
 print(person1[2][0])
+
+coll = {'a', 'e', 'i', 'o', 'u', 'U', 'a'}
+print(coll)
+print(sorted(coll))
+#
