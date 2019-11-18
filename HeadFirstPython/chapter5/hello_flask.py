f3096929c9637fecae3c89e0adc53ade7a8a622f
yinhaijun <yinhaijun@jingdaka.com>
2019-11-18 11:16:27 +0800

    doc(chapter5) 添加Flask文档
    添加Flask文档

--- HeadFirstPython/chapter5/hello_flask.py
+++ HeadFirstPython/chapter5/hello_flask.py
@@ -30,4 +30,5 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
